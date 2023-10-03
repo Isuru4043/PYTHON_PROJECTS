082c1f02e163943222857355061eafa11c66c0d7
Isuru Akalanka <isuru.akalanka4043@gmail.com>
2023-10-03 19:25:13 +0530

    created the guessing number game_computer

--- guess_the_no_user.py
+++ guess_the_no_user.py
@@ -12,3 +12,25 @@ def guess(x):
         elif guess > random_number:
             print("Sorry, guess again, Too high.")
     print(f'you guessed  {random_number}')
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'is {guess} lower than(l), higher than(h) or equal(c)'.lower())
+        if feedback == 'l':
+            low = guess+1
+        elif feedback == 'h':
+            high = guess-1
+
+    print("You guessed the number correctly")
+
+
+
+
