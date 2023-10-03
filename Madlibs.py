81d74da8586111d9a53bad3bbaa25c67bbebd80b
Isuru Akalanka <isuru.akalanka4043@gmail.com>
2023-10-03 17:17:13 +0530

    Created madlibs

---
+++ Madlibs.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+verb1 = input("verb01: ")
+verb2 = input("verb02: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"computer programming is so {adj}! It makes me so exited all the time because " \
+         f"I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}"
