fb40875ac4156330446654ba1a538fcc3a5962ea
Isuru4043 <isuru.akalanka4043@gmail.com>
2023-10-10 11:07:02 +0530

    create make_moveMethod

--- game.py
+++ game.py
@@ -4,15 +4,37 @@ class TicTacToe:
         self.current_winner = None
 
     def print_board(self):
-        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
-        number_board = [[str(i) for i in range(j*3,(j+1)*3)] for j in range(3)]
+        number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
     def availableSpaces(self):
         return [i for (i, spot) in enumerate(self.board) if spot == " "]
 
+    def emptySquares(self):
+        return ' ' in self.board
+
+    def make_move(self, square, letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+
+    def num_of_emptySquares(self):
+        return self.board.count(' ')
+
+
+def play(game, x_player, O_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'
+
+    while game.emptySquares():
+        if letter == 'O':
+            square = O_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
